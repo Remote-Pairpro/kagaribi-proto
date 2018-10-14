33a8dcfedd078b644c324be4749abd8264fa7080
Hidari0415 <ika5140@gmail.com>
2018-10-14 22:55:11 +0900

    add navi

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import * as routes from "../../constants/routes";
-import { About } from "../About/About";
-import { Home } from "../Home/Home";
+import About from "../About/About";
+import Home from "../Home/Home";
+import Navigation from "../Navigation/Navigation";
 
 export const Root = () => (
     <BrowserRouter>
         <div>
+            <Navigation />
+            <hr />
+
             <Route exact path={routes.HOME} component={Home} />
             <Route path={routes.ABOUT} component={About} />
         </div>
