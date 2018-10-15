e434f1a32d37573140033949dab3e10ca4d51fb4
Hidari0415 <ika5140@gmail.com>
2018-10-16 00:51:44 +0900

    implementing signup form

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import * as routes from "../../constants/routes";
+import { auth } from "../../firebase/firebase";
 
 interface ISignUpFormState {
     username: string;
@@ -9,6 +10,10 @@ interface ISignUpFormState {
     passwordTwo: string;
     error: any;
 }
+
+// const byPropKey = (propertyName: string, value: any) => ({
+//     [propertyName]: value,
+// });
 
 class SignUpForm extends React.Component<{}, ISignUpFormState> {
     constructor({}) {
@@ -27,7 +32,52 @@ class SignUpForm extends React.Component<{}, ISignUpFormState> {
     };
 
     public render() {
-        return <form onSubmit={this.onSubmit} />;
+        const states: ISignUpFormState = this.state;
+        const isInvalid: boolean =
+            states.passwordOne !== states.passwordTwo ||
+            states.passwordOne === "" ||
+            states.email === "" ||
+            states.username === "";
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    value={states.username}
+                    onChange={event =>
+                        this.setState({ username: event.target.value })
+                    }
+                    type="text"
+                    placeholder="Full Name"
+                />
+                <input
+                    value={states.email}
+                    onChange={event =>
+                        this.setState({ email: event.target.value })
+                    }
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    value={states.passwordOne}
+                    onChange={event =>
+                        this.setState({ passwordOne: event.target.value })
+                    }
+                    type="password"
+                    placeholder="Password"
+                />
+                <input
+                    value={states.passwordTwo}
+                    onChange={event =>
+                        this.setState({ passwordTwo: event.target.value })
+                    }
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button type="submit" disabled={isInvalid}>
+                    Sign Up
+                </button>
+                {states.error && <p>{states.error.message}</p>}
+            </form>
+        );
     }
 }
 
