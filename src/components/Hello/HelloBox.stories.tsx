a0f00474e023a71e80b455c8bc3e1913a730843d
Hidari0415 <ika5140@gmail.com>
2018-10-27 01:30:30 +0900

    delete experimental component

--- src/components/Hello/HelloBox.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import * as React from "react";
-import { HelloBox } from "./HelloBox";
-
-storiesOf("HelloBox", module).add("default", () => {
-    return <HelloBox compiler="TypeScript" framework="React" />;
-});
