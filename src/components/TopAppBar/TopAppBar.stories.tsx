f66ed7774964d461d24128aa7afced908f9f189b
Hidari0415 <ika5140@gmail.com>
2018-11-04 15:29:04 +0900

    fix stories

--- src/components/TopAppBar/TopAppBar.stories.tsx
+++ src/components/TopAppBar/TopAppBar.stories.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import { firebaseUserDummy } from "../../test-helpers/firebase-stub";
 import TopAppBar from "./TopAppBar";
 
-storiesOf("TopAppBar", module).add("Non Auth", () => {
-    return <TopAppBar authUser={null} />;
-});
-
-storiesOf("TopAppBar", module).add("Auth", () => {
-    return <TopAppBar authUser={firebaseUserDummy} />;
-});
+storiesOf("TopAppBar", module)
+    .add("Non Auth", () => {
+        return <TopAppBar authUser={null} />;
+    })
+    .add("Auth", () => {
+        return <TopAppBar authUser={firebaseUserDummy} />;
+    });
