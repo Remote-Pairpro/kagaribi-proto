ddb790ade176f3b435cae44aa82fea12e9442cd3
Hidari0415 <ika5140@gmail.com>
2018-11-10 02:29:32 +0900

    add link to HOME in AppBar

--- src/components/TopAppBar/TopAppBar.stories.tsx
+++ src/components/TopAppBar/TopAppBar.stories.tsx
@@ -7,13 +7,17 @@ import TopAppBar from "./TopAppBar";
 
 storiesOf("TopAppBar", module)
     .addDecorator(story => (
-        <MemoryRouter initialEntries={[routes.SIGN_IN]}>{story()}</MemoryRouter>
+        <MemoryRouter initialEntries={[routes.HOME, routes.SIGN_IN]}>
+            {story()}
+        </MemoryRouter>
     ))
     .add("Non Auth", () => {
         return <TopAppBar authUser={null} />;
     })
     .addDecorator(story => (
-        <MemoryRouter initialEntries={[routes.MY_PAGE]}>{story()}</MemoryRouter>
+        <MemoryRouter initialEntries={[routes.HOME, routes.MY_PAGE]}>
+            {story()}
+        </MemoryRouter>
     ))
     .add("Auth", () => {
         return <TopAppBar authUser={firebaseUserDummy} />;
--- src/components/TopAppBar/TopAppBar.tsx
+++ src/components/TopAppBar/TopAppBar.tsx
@@ -4,12 +4,17 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
 import React from "react";
+import { Link } from "react-router-dom";
+import * as routes from "../../constants/routes";
 import TopAppBarAccountButton from "./TopAppBarAccountButton";
 import TopAppBarSignInButton from "./TopAppBarSignInButton";
 
 interface ITopAppBarProps {
     authUser: firebase.User;
 }
+
+// See https://material-ui.com/demos/buttons/#third-party-routing-library
+const LinkToHome = (prop: any) => <Link to={routes.HOME} {...prop} />;
 
 class TopAppBar extends React.Component<ITopAppBarProps> {
     constructor(appTopBarProps: ITopAppBarProps) {
@@ -25,7 +30,12 @@ class TopAppBar extends React.Component<ITopAppBarProps> {
                     <IconButton color="inherit">
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" color="inherit">
+                    <Typography
+                        variant="h6"
+                        color="inherit"
+                        component={LinkToHome}
+                        style={{ textDecoration: "none" }}
+                    >
                         Kagaribi-proto
                     </Typography>
                     {user ? (
