d5e67a7539dabd2e552d6a4360a72d84f51a7a87
Hidari0415 <ika5140@gmail.com>
2018-11-10 22:58:39 +0900

    remove unused component

--- src/components/SignOut/SignOutButton.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-import SignOutButton from "./SignOutButton";
-
-storiesOf("SignOut", module).add("default", () => {
-    return <SignOutButton />;
-});
