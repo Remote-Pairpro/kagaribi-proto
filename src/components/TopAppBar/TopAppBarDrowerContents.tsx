4a4f716cf13aacd52f4489620f690ec8705bddf2
Hidari0415 <ika5140@gmail.com>
2018-11-11 23:49:54 +0900

    add link to home and about

--- src/components/TopAppBar/TopAppBarDrowerContents.tsx
+++ src/components/TopAppBar/TopAppBarDrowerContents.tsx
@@ -5,18 +5,24 @@ import ListItemText from "@material-ui/core/ListItemText";
 import HomeIcon from "@material-ui/icons/Home";
 import SubjectIcon from "@material-ui/icons/Subject";
 import React from "react";
+import { Link } from "react-router-dom";
+import * as routes from "../../constants/routes";
+
+// See https://material-ui.com/demos/buttons/#third-party-routing-library
+const HomeLink = (prop: any) => <Link to={routes.HOME} {...prop} />;
+const AboutLink = (prop: any) => <Link to={routes.ABOUT} {...prop} />;
 
 const TopAppBarDrowerContents = () => {
     return (
         <div>
             <List>
-                <ListItem button>
+                <ListItem button component={HomeLink}>
                     <ListItemIcon>
                         <HomeIcon />
                     </ListItemIcon>
                     <ListItemText>Home</ListItemText>
                 </ListItem>
-                <ListItem button>
+                <ListItem button component={AboutLink}>
                     <ListItemIcon>
                         <SubjectIcon />
                     </ListItemIcon>
