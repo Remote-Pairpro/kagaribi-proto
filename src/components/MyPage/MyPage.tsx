f1d8f5137ab7d6fa0d75e94ad9cc894587911148
Hidari0415 <ika5140@gmail.com>
2018-11-17 00:50:54 +0900

    remove unused import

--- src/components/MyPage/MyPage.tsx
+++ src/components/MyPage/MyPage.tsx
@@ -1,6 +1,5 @@
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import EmailIcon from "@material-ui/icons/Email";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
