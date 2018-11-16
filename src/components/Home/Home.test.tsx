42d02b4bbd55a8329dde8e4e101cf2c9e276dd48
Hidari0415 <ika5140@gmail.com>
2018-11-17 00:49:45 +0900

    add test for Home

---
+++ src/components/Home/Home.test.tsx
@@ -0,0 +1,17 @@
+import { render, shallow } from "enzyme";
+import React from "react";
+import renderer from "react-test-renderer";
+import Home from "./Home";
+
+describe("Home Test", () => {
+    it("static contents test", () => {
+        const wrapper = render(<Home />);
+        expect(wrapper.text()).toContain("Home");
+        expect(wrapper.text()).toContain("ようこそ Kagaribi へ");
+    });
+
+    it("renders Home correctly", () => {
+        const tree = renderer.create(<Home />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
