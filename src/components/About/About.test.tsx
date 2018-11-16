b177d744adbefe1d779d8413fbe4861dffd95780
Hidari0415 <ika5140@gmail.com>
2018-11-17 00:27:17 +0900

    add jest and enzyme with snapshot

---
+++ src/components/About/About.test.tsx
@@ -0,0 +1,24 @@
+import { render, shallow } from "enzyme";
+import React from "react";
+import renderer from "react-test-renderer";
+import About from "./About";
+
+describe("About", () => {
+    it("About test", () => {
+        const wrapper = shallow(<About />);
+        expect(wrapper.find(".hoge").length).toBe(0);
+    });
+
+    it("static rendered markup", () => {
+        const wrapper = render(<About />);
+        expect(wrapper.text()).toContain("About");
+        expect(wrapper.text()).toContain(
+            "Kagaribi はあなたのスケジュールを照らし、調整を容易にします。"
+        );
+    });
+
+    it("renders About correctly", () => {
+        const tree = renderer.create(<About />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
