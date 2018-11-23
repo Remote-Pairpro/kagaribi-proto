84879b3b66daea721077afba413622bd9f9896be
Hidari0415 <ika5140@gmail.com>
2018-11-23 14:45:40 +0900

    fix typo

--- src/components/SignIn/SignIn.stories.tsx
+++ src/components/SignIn/SignIn.stories.tsx
@@ -2,6 +2,6 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 import SignInForm from "../SignIn/SignInForm";
 
-storiesOf("SignIn", module).add("SignUpForm", () => {
+storiesOf("SignIn", module).add("SignInForm", () => {
     return <SignInForm history={null} />;
 });
